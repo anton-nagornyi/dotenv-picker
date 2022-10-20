77edb3e25907179892314a21e3f03c9f617a6dd0
Anton <anton.nagornyi@gmail.com>
2022-10-20 12:29:56 +0300

    feat: replace env variables with values in cmd

--- src/set-env.ts
+++ src/set-env.ts
@@ -2,12 +2,19 @@ import dotenv from 'dotenv';
 import { execSync } from 'child_process';
 import { baseSelect } from './baseSelect';
 
+const cmdSubstitude = (s: string, vars: { [key: string]: string }) => s.replace(/(\\*)(\$([_a-z0-9]+)|\${([_a-z0-9]+)})/ig, (_, escape, varExpression, variable, bracedVariable) => {
+  if (!(escape.length % 2)) {
+    return escape.substring(Math.ceil(escape.length / 2)) + (vars[variable || bracedVariable] || '');
+  }
+  return escape.substring(1) + varExpression;
+});
+
 const setEnv = (dotEnvPath: string): void => {
   const result = dotenv.config({
     path: dotEnvPath,
   });
 
-  const cmd = process.argv.slice(2).join(' ');
+  const cmd = cmdSubstitude(process.argv.slice(2).join(' '), result.parsed || {});
   const { APPDATA, PATH } = process.env;
   execSync(cmd, { stdio: 'inherit', env: { ...result.parsed, APPDATA, PATH } });
 };
